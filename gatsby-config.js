23590ef406dfc17c443b70118e06bfd0af1666b6
Ivan Bila <ivanbilag@gmail.com>
2019-04-28 19:02:47 +0200

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-139233066-1`,
       },
     },
     `gatsby-plugin-feed`,

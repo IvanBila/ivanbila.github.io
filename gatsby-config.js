19d8da7b1ca072c6cfb09d1abe0dcd00c6424bf3
Ivan Bila <ivanbilag@gmail.com>
2019-12-21 15:10:08 +0200

    Use correct siteURL

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Musings`,
     author: `Ivan Bila`,
     description: `Development musings`,
-    siteUrl: `https://ivanbila.github.io`,
+    siteUrl: `https://ivanbila.dev`,
     social: {
       twitter: `ivanbila`,
     },

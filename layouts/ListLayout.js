d5fadda5705126643107e8516ff97cd6f78a85f7
Ivan Bila <ivanbilag@gmail.com>
2022-08-08 14:02:10 +0200

    Update data

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -67,11 +67,6 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
                           {title}
                         </Link>
                       </h3>
-                      <div className="flex flex-wrap">
-                        {tags.map((tag) => (
-                          <Tag key={tag} text={tag} />
-                        ))}
-                      </div>
                     </div>
                     <div className="prose text-gray-500 max-w-none dark:text-gray-400">
                       {summary}
